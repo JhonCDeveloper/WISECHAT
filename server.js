72ee1f74d810c0ee43e918f8cd0b144c949a1b2e
JhonCDeveloper <jairovalencia100k19@gmail.com>
2024-07-09 07:25:24 -0500

    Web Server

--- server.js
+++ server.js
@@ -3,38 +3,51 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/wisechat')
-    .then(() => console.log('Conectado a MongoDB'))
-    .catch(err => console.error('No se pudo conectar a MongoDB...', err));
+mongoose.connect('mongodb+srv://jairovalencia100k19:Xm4aASH2l8CN8ZYZ@clusterwisechat.myqvx7p.mongodb.net/?retryWrites=true&w=majority&appName=ClusterWiseChat', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Conectado a MongoDB Atlas...'))
+  .catch(err => console.error('No se pudo conectar a MongoDB Atlas...', err));
 
+// Define los esquemas y modelos de MongoDB aquí
 const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 });
 
 const User = mongoose.model('User', userSchema);
 
+// Endpoint para registrar usuarios
 app.post('/api/register', async (req, res) => {
-    const { email, password } = req.body;
-    let user = new User({ email, password });
-    user = await user.save();
-    res.send(user);
+  const { email, password } = req.body;
+  try {
+    const user = new User({ email, password });
+    await user.save();
+    res.status(201).send('Registro exitoso');
+  } catch (error) {
+    res.status(400).send('Error al registrar usuario');
+  }
 });
 
+// Endpoint para iniciar sesión
 app.post('/api/login', async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
+  try {
     const user = await User.findOne({ email, password });
-    if (!user) return res.status(400).send('Correo o contraseña incorrectos');
-    
-    // Generar y enviar token (aquí puedes generar un token JWT si lo deseas)
-    res.send('Inicio de sesión exitoso');
+    if (user) {
+      res.status(200).send('Inicio de sesión exitoso');
+    } else {
+      res.status(400).send('Correo o contraseña incorrectos');
+    }
+  } catch (error) {
+    res.status(500).send('Error al iniciar sesión');
+  }
 });
 
-app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
-});
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
